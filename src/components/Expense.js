727c513f07b68bc2d8cceb560837e1feb50f8fb9
Amitnegi1234 <amitnegi6341@gmail.com>
2023-10-12 18:46:36 +0530

    upload new

---
+++ src/components/Expense.js
@@ -0,0 +1,19 @@
+function Expense(){
+    return(
+        <div>
+            <div>
+                <h3>Food</h3>
+                <div>Rs 30</div>
+            </div>
+            <div>
+                <h3>petrol</h3>
+                <div>Rs 100</div>
+            </div>
+            <div>
+                <h3>Movies</h3>
+                <div>Rs 200</div>
+            </div>
+        </div>
+    );
+}
+ export default Expense;
