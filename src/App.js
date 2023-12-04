7b5256e4efe359285e4dc087f0d7fca6485e9d1a
Amitnegi1234 <amitnegi6341@gmail.com>
2023-12-04 19:05:33 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
+import React, { useState } from 'react';
 import './App.css';
 import Expense from './components/Expense';
-import NewExpense  from './components/NewExpense/NewExpense';
+import NewExpense from './components/NewExpense/NewExpense';
 
 function App() {
-  const expenses = [
+  const [expenses, setExpenses] = useState([
     {
       id: 'e1',
       title: 'Toilet Paper',
@@ -24,33 +24,50 @@ function App() {
       amount: 450,
       date: new Date(2021, 5, 12),
     },
-  ];
+  ]);
+
+  const [selectedYear, setSelectedYear] = useState('');
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => [expense, ...prevExpenses]);
+  };
+  
+  const yearChangeHandler = (event) => {
+   
+    setSelectedYear(event.target.value);
+  };
+  const filteredExpenses = selectedYear
+  ? expenses.filter(
+      (expense) => new Date(expense.date).getFullYear().toString() === selectedYear
+    )
+  : expenses;
 
   return (
     <div>
-      <NewExpense/>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <h1>Lets get Started</h1>
-      <Expense title={expenses[0].title} 
-             amount={expenses[0].amount}
-             date={expenses[0].date}></Expense>
-      <Expense  title={expenses[1].title} 
-             amount={expenses[1].amount}
-             date={expenses[1].date}></Expense>
-      <Expense  title={expenses[2].title} 
-             amount={expenses[2].amount}
-             date={expenses[2].date}></Expense>
-      <Expense  title={expenses[3].title} 
-             amount={expenses[3].amount}
-             date={expenses[3].date}></Expense>
+      <form action="">
+  <select id="cars" name="cars" onChange={yearChangeHandler} value={selectedYear}>
+    <option value="">All Years</option>
+    <option value="2019">2019</option>
+    <option value="2020">2020</option>
+    <option value="2021">2021</option>
+    <option value="2022">2022</option>
+  </select>
+</form>
+
+   {filteredExpenses.length===0 && <p>no expenses found</p>}
+      {filteredExpenses.length > 0 && (
+        filteredExpenses.map((expense) => (
+          <Expense
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))
+      )}
       
-      {/* {expenses.map((expense) => (
-        <Expense
-          key={expense.id} // It's important to provide a unique key when rendering a list of components.
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))} */}
     </div>
   );
 }
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,48 +1,58 @@
 import React ,{useState} from "react";
 
 import './NewExpense.css';
-const NewExpense=()=>{
-   const [enteredTitle,setEnteredTitle]=useState('');
-   const [enteredAmount,setEnteredAmount]=useState('');
-   const [enteredDate,setEnteredDate]=useState('');
 
-    const titleChangeHandler =(event)=>{
-        setEnteredTitle(event.target.value)
-    }
-    const amountChangeHandler =event =>{
-        setEnteredAmount(event.target.value)
-    }
-    const dateChangeHandler =event =>{
-        setEnteredDate(event.target.value)
-    }
+const NewExpense = (props) => {
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
 
-    const submithandler = (event) =>{
-        event.preventDefault();
-        const expenseData = {
-            title: enteredTitle,
-            amount: enteredAmount,
-            date:new Date(enteredDate)
-        };
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+  };
 
-        console.log(expenseData);
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate)
     };
+
+    // Pass the expenseData to the parent component
+    props.onAddExpense(expenseData);
+
+    // Clear the form inputs
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
+  };
 return( 
 <div>
-    <form onSubmit={submithandler}>
+    <form  onSubmit={submitHandler}>
         <div className="New_Expense_controls">
             <div className="New_Expense_control form-control">
                 <label>Title</label>
-                <input type="text" onChange={titleChangeHandler}/>
+                <input type="text" value={enteredTitle} onChange={titleChangeHandler}/>
             </div>
 
             <div className="New_Expense_control form-control">
                 <label>Amount</label>
-                <input type="number" min={'0.01'} step={'0.01'} onChange={amountChangeHandler}/>
+                <input type="number" min={'0.01'} step={'0.01'} value={enteredAmount} onChange={amountChangeHandler}/>
             </div>
 
             <div className="New_Expense_control form-control">
                 <label>Date</label>
-                <input type="date" min={'2020-3-20'} max={'2023-10-15'} onChange={dateChangeHandler}/>
+                <input type="date" min={'2020-3-20'} max={'2023-10-15'} value={enteredDate} onChange={dateChangeHandler}/>
             </div>
 
             <div className="New_Expense_actions">
